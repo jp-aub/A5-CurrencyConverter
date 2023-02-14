ae154319fe361460bea8a5320588fb360025d121
jp-aub <109827323+jp-aub@users.noreply.github.com>
2023-02-14 12:14:31 -0500

    Added second view controller and additional UIViewController. Added elements in second UIViewController.

---
+++ A5-CurrencyConverter/CurrencyConvertedView.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyConvertedView.swift
+//  A5-CurrencyConverter
+//
+//  Created by JP on 2/14/23.
+//
+
+import UIKit
+
+class CurrencyConvertedView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
