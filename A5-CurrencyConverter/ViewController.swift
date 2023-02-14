59f544e83de18c33e81a7004669fe14d20a423ba
jp-aub <109827323+jp-aub@users.noreply.github.com>
2023-02-14 09:58:24 -0500

    Initial Commit

---
+++ A5-CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A5-CurrencyConverter
+//
+//  Created by JP on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
